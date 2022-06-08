43e6c27f9e2a36ef8461ca0b7eecfca91d20859a
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-08 12:23:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -57,7 +57,7 @@ free: 15,
 
 //—————「 Set Image 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.thumb = fs.readFileSync('./image/chika.jpg')
+global.thumb = fs.readFileSync('./image/yahya.png')
 /*global.veloriy = fs.readFileSync('./image/chika.mp4')*/
 
 //—————「 Set Image Welcome 」—————//
